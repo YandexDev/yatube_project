bf4f36459d0223361b34bf3cf38237c35afce0b9
Andrei Pervyi <taxidevelop@yandex.ru>
2021-12-01 11:42:37 +0400

    Update templates

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -3,9 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # Главная страница
     path('', views.index),
-    # Страницы сообществ ждем переменную slug типа slug
-    # Конвертор тип slug - строки и положиетельные числа
-    # path('group/<slug:slug>/', views.group_posts), Пока не создана функция group_posts
+    path('index/', views.index, name='index'),
+    path('group_list/', views.group_list, name='group_list'),
 ]
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -4,3 +4,8 @@ from django.shortcuts import render
 def index(request):
     template = 'posts/index.html'
     return render(request, template)
+
+
+def group_list(request):
+    template = 'posts/group_list.html'
+    return render(request, template)
